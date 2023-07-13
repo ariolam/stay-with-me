0dd2bcfcf933dd9f4b0819b6a11b679a7bec9a32
ariola <ariola.markou@gmail.com>
2023-07-14 01:20:14 +0300

    validate username and password

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -56,13 +56,54 @@ router.get("/userProfile", (req, res) => res.render("users/user-profile"));
 router.get("/login", (req, res) => res.render("auth/login"));
 
 // POST login route ==> to process form data
-router.post("/login", (req, res, next) => {
+router.post("/login", async (req, res, next) => {
     // console.log(req.body);
     if (username === "" || password === "") {
         res.render("auth/login", {
             errorMessage: "Please enter both, username and password to login.",
         });
         return;
+    }
+    try {
+        const userData = req.body;
+        const checkedUser = await User.findOne({
+            username: userData.username,
+        });
+        // Check is user does exists in DB
+        if (checkedUser) {
+            // if so, compare the password with the passwordHash in DB
+            if (
+                bcryptjs.compareSync(
+                    userData.password,
+                    checkedUser.passwordHash
+                )
+            ) {
+                // If password is correct
+                const loggedUser = { ...checkedUser._doc };
+                delete loggedUser.passwordHash;
+                res.redirect("/profile");
+            } else {
+                // If password is incorrect
+                console.log("Password is incorrect");
+                res.render("auth/login", {
+                    errorMessage: "Password is incorrect",
+                    payload: { username: userData.username },
+                });
+            }
+        } else {
+            // No user with this email
+            console.log("No user with this email");
+            res.render("auth/login", {
+                errorMessage: "No user with this email",
+                payload: { username: userData.username },
+            });
+        }
+    } catch (error) {
+        console.log("error occured: ", error);
+        res.render("auth/login", {
+            errorMessage: "There was an error on the server",
+            payload: { username: userData.username },
+        });
     }
 });
 
