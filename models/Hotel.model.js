7bb25b6965bf08dbb5e5f87594c8be22241604a6
ariola <ariola.markou@gmail.com>
2023-07-18 17:47:43 +0300

    update hotel model

--- models/Hotel.model.js
+++ models/Hotel.model.js
@@ -1,29 +1,38 @@
 const { Schema, model } = require("mongoose");
 
 const hotelSchema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: false,
-  },
-  guest: {
-    type: Number,
-    required: true,
-  },
-  pricePerNight: {
-    type: Number,
-    required: true,
-  },
-  location: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-    default:
-      "https://www.tripadvisor.com/HotelsList-Sri_Lanka-Luxury-Beach-Resorts-zfp7946468.html",
-    required: true,
-  },
+    name: {
+        type: String,
+        trim: true,
+        required: false,
+    },
+    guest: {
+        type: Number,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+        required: true,
+    },
+    nights: {
+        type: Number,
+        required: true,
+    },
+    pricePerNight: {
+        type: Number,
+        required: true,
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        default:
+            "https://www.tripadvisor.com/HotelsList-Sri_Lanka-Luxury-Beach-Resorts-zfp7946468.html",
+        required: true,
+    },
 });
 
 const Hotel = model("Hotel", hotelSchema);
